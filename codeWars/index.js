53136447d25ee274a3e9af23f5f247c24a0d1c2f
Andrei Naumenko <6110814n@gmail.com>
2022-09-23 09:58:03 +0300

    fix: add task

--- codeWars/index.js
+++ codeWars/index.js
@@ -28,3 +28,24 @@
 // } 
 // runLengthEncoding("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW"); //,[[12,'W'],[1,'B'],[12,'W'],[3,'B'],[24,'W'],[1,'B'],[14,'W']]
 
+// function noSpace(x){
+//   let result = '';
+//   for(let i = 0; i < x.length; i++) {
+//     if(x[i] !== ' ') {
+//       result += x[i];
+//     }
+//   }
+//   console.log(result);
+//   return result;
+// }
+// noSpace('8 j 8   mBliB8g  imjB8B8  jl  B');//, '8j8mBliB8gimjB8B8jlB');
+
+// var summation = function (num) {
+//   let sum = 0;
+//   for(let i = 1; i <= num; i++) {
+//     sum += i
+//   }
+//   console.log(sum);
+//   return sum;
+// }
+// summation(8);//, 36)
